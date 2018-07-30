6a775f751e8c2b7c140fc5d54e203b75219c9100
Amrender-singh <amrendersingh1995@gmail.com>
2018-07-30 21:35:41 +0530

    Add test cases for get todo by id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,10 +1,13 @@
+const {ObjectID} = require('mongodb');
 const expect = require('expect');
 const request = require('supertest');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 //test data
+var objectId = new ObjectID();
 const todos = [{
+    _id : objectId,
     text : "First test todo"
 },{
     text : "Second test todo"
@@ -57,5 +60,29 @@ describe('get /todos', ()=>{
         .expect((res)=>{
             expect(res.body.todos.length).toBe(2);
         }).end(done);
+    });
+});
+describe('get /todos:id', ()=>{
+    it('Should get  Todos with given id', (done)=>{
+        request(app)
+        .get(`/todos/${objectId.toHexString()}`)
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todo.text).toBe(todos[0].text);
+        }).end(done);
+    });
+    it('Should return 404 if Todo not found', (done)=>{
+        var hexId = new ObjectID().toHexString();
+        request(app)
+        .get(`/todos/${hexId}`)
+        .expect(404)
+        .end(done);
+    });
+    it('Should return 404 if not valid Object Id', (done)=>{
+        var hexId = new ObjectID().toHexString();
+        request(app)
+        .get('/todos/123abcd')
+        .expect(404)
+        .end(done);
     });
 });
