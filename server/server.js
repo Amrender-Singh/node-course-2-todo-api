9549bc95477b9489f62b9caa357efe7b0474b677
Amrender-singh <amrendersingh1995@gmail.com>
2018-07-31 22:22:14 +0530

    Add delete todos by id route

--- server/server.js
+++ server/server.js
@@ -43,6 +43,19 @@ app.get("/todos/:id", (req, res)=>{
         res.status(400).send();
     });
 });
+
+app.delete("/todos/:id", (req, res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id))
+        return res.status(404).send();
+    Todo.findByIdAndRemove(id).then((doc)=>{
+        if(!doc)
+            return res.status(404).send();
+        res.send(doc)
+    }, (err)=>{
+        res.status(400);
+    });
+});
 app.listen(port, ()=>{
     console.log(`Started on port ${port}`);
 });
